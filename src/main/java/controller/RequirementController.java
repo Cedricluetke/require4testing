80b521b715c72e7f6fc93e9852dece7b975685ed
cedricluetke <cedricluetke@gmail.com>
2025-12-30 16:42:03 +0100

    Requirement controller implemented

---
+++ src/main/java/controller/RequirementController.java
@@ -0,0 +1,23 @@
+package de.require4testing.controller;
+
+import de.require4testing.model.Requirement;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class RequirementController {
+
+    @GetMapping("/requirements")
+    public String showRequirements(Model model) {
+
+        Requirement req = new Requirement(
+                1L,
+                "MVP muss laufen",
+                "Der Benutzer soll eine Anforderung per HTML angezeigt bekommen."
+        );
+
+        model.addAttribute("requirement", req);
+        return "requirements";
+    }
+}
