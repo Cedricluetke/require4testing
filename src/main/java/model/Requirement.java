4b8f4cdacb5c59580b583adde909fc772b72649f
cedricluetke <cedricluetke@gmail.com>
2025-12-30 16:30:28 +0100

    Requirement model implemented

---
+++ src/main/java/model/Requirement.java
@@ -0,0 +1,25 @@
+package de.require4testing.model;
+
+public class Requirement {
+
+    private Long ID;
+    private String title;
+    private String description;
+
+    public Requirement(Long ID, String title, String description) {
+        this.ID = ID;
+        this.title = title;
+        this.description = description;
+    }
+
+    public Long getID() {
+        return ID;
+    }
+    public String getTitle() {
+        return title;
+    }
+    public String getDescription() {
+        return description;
+    }
+}
+
