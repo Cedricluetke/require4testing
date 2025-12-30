9281adaaab568c1b097156eeedb61560f742a2dd
cedricluetke <cedricluetke@gmail.com>
2025-12-30 17:14:44 +0100

    fixed path finding issues

--- src/main/java/de/require4testing/require4testing/model/Requirement.java
+++ src/main/java/de/require4testing/require4testing/model/Requirement.java
@@ -1,19 +1,19 @@
-package de.require4testing.require4testing.model;
+package de.require4testing.model;
 
 public class Requirement {
 
-    private Long ID;
+    private Long id;
     private String title;
     private String description;
 
     public Requirement(Long ID, String title, String description) {
-        this.ID = ID;
+        this.id = ID;
         this.title = title;
         this.description = description;
     }
 
-    public Long getID() {
-        return ID;
+    public Long getId() {
+        return id;
     }
     public String getTitle() {
         return title;
