e9f3111d2c2736d422a014da77c1457868d83776
cedricluetke <cedricluetke@gmail.com>
2026-01-01 11:49:47 +0100

    implemented "completed" boolean for Testcase

--- src/main/java/de/require4testing/require4testing/model/Testcase.java
+++ src/main/java/de/require4testing/require4testing/model/Testcase.java
@@ -3,10 +3,12 @@ package de.require4testing.require4testing.model;
 public class Testcase {
     private Long id;
     private String title;
+    private boolean completed;
 
     public Testcase(Long id, String title) {
         this.id = id;
         this.title = title;
+        completed = false;
     }
 
     public Long getId() {
@@ -23,5 +25,13 @@ public class Testcase {
 
     public void setTitle(String title) {
         this.title = title;
+    }
+
+    public boolean isCompleted() {
+        return completed;
+    }
+
+    public void setCompleted(boolean completed) {
+        this.completed = completed;
     }
 }
