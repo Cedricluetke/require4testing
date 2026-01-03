30e22861b41639e07d5aeaf9a86af8f2e9ff7285
cedricluetke <cedricluetke@gmail.com>
2026-01-03 19:45:53 +0100

    Passwörter in Enum hinterlegt + Logik

--- src/main/java/de/require4testing/require4testing/model/Rolle.java
+++ src/main/java/de/require4testing/require4testing/model/Rolle.java
@@ -1,9 +1,19 @@
 package de.require4testing.require4testing.model;
 
 public enum Rolle {
-    ADMIN,
-    REQUIREMENTS_ENGINEER,
-    TESTMANAGER,
-    TESTFALLERSTELLER,
-    TESTER
+    ADMIN("admin123"),
+    REQUIREMENTS_ENGINEER("re123"),
+    TESTMANAGER("tm123"),
+    TESTFALLERSTELLER("tf123"),
+    TESTER("tester123");
+
+    private final String password;
+
+    Rolle(String password) {
+        this.password = password;
+    }
+
+    public boolean matchesPassword(String input) {
+        return password.equals(input);
+    }
 }
